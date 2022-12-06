e908fd901e4fb4728e1daa37447deb66a9567560
James Shinner <64267552+jamesdshinner@users.noreply.github.com>
2022-12-06 09:48:04 +0000

    Add template

---
+++ dayX.py
@@ -0,0 +1,20 @@
+# Functions
+
+
+def main():
+    # Get input
+    with open("dayX/input.txt", 'r') as f:
+        nums = [line for line in f.readlines()]
+
+    # Parse input
+
+    # Part 1
+
+    print("Part 1:")
+
+    # Part 2
+
+    print("Part 2:")
+
+if __name__ == '__main__':
+    main()
